a85509c672c899ba2f1cd641659051dc6001bb14
iimosh <imantopcii@gmail.com>
2025-01-11 23:00:10 +0100

    created tickerlist project

---
+++ Hw4/TickerList/src/main/java/mk/ukim/finki/tickerlist/TickerListApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.tickerlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickerListApplication.class, args);
+    }
+
+}
