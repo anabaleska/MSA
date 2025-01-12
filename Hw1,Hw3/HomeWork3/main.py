9702ce65fbc3eb1ad99fc8039a8119aabeb6852a
anabaleska <anabaleska5@gmail.com>
2025-01-13 00:07:49 +0100

    docker again

--- Hw1,Hw3/HomeWork3/main.py
+++ Hw1,Hw3/HomeWork3/main.py
@@ -1,6 +1,6 @@
-from .sentiment import init
-from .indicators import process_all_tickers
-from .fetch_full_names import main
+from sentiment import init
+from indicators import process_all_tickers
+from fetch_full_names import main
 
 if __name__ == '__main__':
     main()
